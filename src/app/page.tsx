58d05cf699a28cd3497007f626012d7e1e90be5c
Hong Linh <honglinh11032004@gmail.com>
2026-08-27 11:57:05 +0700

    chore: remove SystemBenefitsSection from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { HeroBanner } from "@/components/home/HeroBanner";
 import { QrStoryAnimationSection } from "@/components/home/QrStoryAnimationSection";
-import { SystemBenefitsSection } from "@/components/home/SystemBenefitsSection";
 import NewsSection from "@/components/home/NewsSection";
 
 export default function HomePage() {
@@ -8,7 +7,6 @@ export default function HomePage() {
         <main className="mx-auto min-h-screen max-w-6xl px-4 py-6 sm:px-6 lg:px-8 space-y-10">
             <HeroBanner />
             <QrStoryAnimationSection />
-            <SystemBenefitsSection />
             <NewsSection />
         </main>
     );
